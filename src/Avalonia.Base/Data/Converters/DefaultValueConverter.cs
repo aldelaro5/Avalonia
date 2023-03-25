ee03831b748ff9c394ccdde3950d45dcebb1eca2
aldelaro5 <aldelaro5@gmail.com>
2023-03-25 03:27:55 -0400

    Convert null to Unset if it's a value type that is not Nullable<T>

--- src/Avalonia.Base/Data/Converters/DefaultValueConverter.cs
+++ src/Avalonia.Base/Data/Converters/DefaultValueConverter.cs
@@ -30,7 +30,7 @@ namespace Avalonia.Data.Converters
         {
             if (value == null)
             {
-                return null;
+                return targetType.IsValueType && Nullable.GetUnderlyingType(targetType) is null ? AvaloniaProperty.UnsetValue : null;
             }
 
             if (typeof(ICommand).IsAssignableFrom(targetType) && value is Delegate d && d.Method.GetParameters().Length <= 1)
